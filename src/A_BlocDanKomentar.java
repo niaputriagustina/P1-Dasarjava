8ead181c12ede4920402611ba731406192c89f81
PC 10 <niaputriagustina12082000@gmail>
2022-03-14 15:23:49 +0700

    Intial project pertemuan 1 dasar java

---
+++ src/A_BlocDanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlocDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Dasar pemograman java");
+
+
+        //contoh komentar 1 baris
+         /*
+        Contoh komentar lebih dari 1 baris
+        */
+        /**
+         * java doc -->baris ini untuk mendokumtasikan kode program
+
+         */
+    }
+}
